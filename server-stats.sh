288c4691bb9c3bddd1e3fcc7a08c620c0a6ba747
ibewilson@yahoo.com <wilexy@DESKTOP-VC8QHSS.localdomain>
2025-03-09 19:34:44 +0000

    Added CPU usage measurement

--- server-stats.sh
+++ server-stats.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 
-echo "Hello, World! This is where the Server Performance Stats script starts."
+# This gets CPU usage using 'top' and 'awk'
 
+CPU_USAGE=$(top -bn1 | grep "Cpu(s)" | awk '{print $2 + $4}')
+
+# Display CPU Usage
+echo "++++++++++++++++++++++++++++++++++++++"
+echo "       SERVER PERFORMANCE STATS      "
+echo "++++++++++++++++++++++++++++++++++++++"
+echo ""
+echo "1. CPU Usage:"
+printf "   Current CPU Usage: %.1f%%\n" "$CPU_USAGE"
+echo ""
